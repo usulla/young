22c3013d43d1dc52656e9257276f66fd8b8caebe
Usulla <maria_suxareva@mail.ru>
2018-02-19 22:26:10 +0300

    Add project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+	$('.owl-carousel').owlCarousel({
+		items:1,
+		nav: true,
+		navText: [
+		"<img src='//dev-cdn.tnt-online.ru/super/img/prev.png'>",
+		"<img src='//dev-cdn.tnt-online.ru/super/img/next.png'>"
+		]
+	});
+	console.log('dsds')
+}); // DOMContentLoaded
